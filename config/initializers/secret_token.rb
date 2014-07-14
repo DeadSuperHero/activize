a21b81f58ed1be4bb3fd0f99cce530ea31c48069
DeadSuperHero <sean@deadsuperhero.com>
2014-07-14 17:03:37 -0500

    Starting over. Sad it has to be this way, but doing things out of order will screw you up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Activize::Application.config.secret_key_base = '0ffda7844a658ea944562f53161727324b1983a5b38578a6c33b758023c723955f63f55b4ae9d616e8b1841434aae66d680aaa0d0d6f2ba2403997f9be3126ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Activize::Application.config.session_store :cookie_store, key: '_activize_session'
