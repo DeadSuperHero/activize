d853088f066d670db557043534faee10a6061a30
DeadSuperHero <sean@deadsuperhero.com>
2014-07-15 02:11:54 -0500

    System now supports Libravatar as well as Gravatar out of the box.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ def markdown(text)
   markdown.render(text).html_safe
 end
 
+def avatar_for(user, options = { size:50})
+	avatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+	"https://seccdn.libravatar.org/avatar/#{avatar_id}.png"
+end
+
 end
